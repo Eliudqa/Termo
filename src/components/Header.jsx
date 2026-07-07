53fbd36b5585e62d03bf557a3782ab7057b593df
Eliudqa <157858774+Eliudqa@users.noreply.github.com>
2026-07-06 23:03:20 -0500

    fix_super_final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       </div>
       <h1 className="hxs-h1">Resuelve tu ejercicio de intercambiadores</h1>
       <p className="hxs-sub">
-        Pega el enunciado, La IA extrae los datos y este motor aplica LMTD o efectividad-NTU
+        Pega el enunciado, extraemos los datos y este motor aplica LMTD o efectividad-NTU
          sin que tengas que teclear un solo número.
       </p>
     </>
